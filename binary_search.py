d04bf7cf35dbaaf3fa012e1769f270296b43b031
twinkle <you@example.com>
2019-06-11 16:01:02 +0530

    use  if conditions

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,22 @@
-binary_search=[3,4,8,23,56,78,90,78]
-start=0
-end=len(binary_search)
-mid_index=start+end/2
-while(start<end)
 
+binary_search=[3,4,8,23,56,78,90]
+start=0
+end=len(binary_search)-1
+mid=start+end/2
+user_input=int(raw_input("enter no."))
+while(start<end):
+    
+
+    mid=start+end/2
+    if(user_input==binary_search[mid]):
+        print mid
+    
+    elif(user_input<binary_search[mid]):
+        start=mid-1
+        
+    
+    
+    elif(user_input>binary_search[mid]):
+        end=mid+1
+    start=start+1 
+print start  
